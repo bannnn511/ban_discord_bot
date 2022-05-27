74a96fa4cd90cdf597977db37169e3beecf7c074
An <habinhan1999@gmail.com>
2022-05-27 13:25:36 +0700

    change type

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = commands.Bot(command_prefix=os.environ.get('PREFIX'))
-
+subscribed_channel=os.environ.get('SUBSRIBED_CHANNEL')
 
 @client.event
 async def on_message(message):
@@ -18,8 +18,8 @@ async def on_message(message):
     # Ignore messages made by the bot
     if(message.author == client.user):
         return
-    if (message.channel.id==os.environ.get('ANNOUCE_CHANNEL')):
-       await client.get_channel(os.environ.get('SUBSRIBED_CHANNEL')).send(message.content)
+    if ((str(message.channel.id))==os.environ.get('ANNOUCE_CHANNEL')):
+        await client.get_channel(int(subscribed_channel)).send(message.content)
 
 
 client.run(os.environ.get('TOKEN'))
